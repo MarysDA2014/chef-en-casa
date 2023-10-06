eb992b75e341f8fbe0412a52f29aff71ec1f9a3e
mariana <marysdavida2014@gmail.com>
2023-10-05 19:51:06 -0500

    assets

---
+++ lib/widgets/tarjetacomida.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Tarjetacomida extends StatelessWidget {
+  final String puntaje="5";
+  const Tarjetacomida({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top:270),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        image: DecorationImage(
+            image:AssetImage("assets/imagenes/sanduche.jpeg"),
+            fit: BoxFit.cover
+        ),
+      ),
+      width: 300,
+      height: 350,
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            children: [
+              Text("Delicioso sanduche", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25),),
+              Row( children: [
+                Icon(Icons.star,color: Colors.yellow,),
+                Text(puntaje as String,style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20))
+              ],)
+            ],),
+          CircleAvatar(
+            child: Icon(Icons.favorite, color: Colors.redAccent,),
+            backgroundColor: Colors.white,
+            radius: 16,
+          )
+        ],
+      ),
+
+    );
+  }
+}
