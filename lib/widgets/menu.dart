fa44c94bc47230fb04935bdd013244e8396c2253
laura <laura.lara2034@gmail.com>
2023-10-05 19:52:22 -0500

    Base para el menu

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -10,8 +10,22 @@ class Menu extends StatefulWidget {
 class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
 
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1B2551),
+
+        leading: IconButton(
+          onPressed: () {},
+          icon: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+        ),
+
+
+
+      ),
 
     );
   }
