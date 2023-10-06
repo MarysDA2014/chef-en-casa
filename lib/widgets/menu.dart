0d04f3c373245a57cd351a3634e8049962c833c8
laura <laura.lara2034@gmail.com>
2023-10-05 20:39:52 -0500

    menu

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -12,20 +12,32 @@ class _MenuState extends State<Menu> {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1B2551),
-
-        leading: IconButton(
-          onPressed: () {},
-          icon: IconButton(
-            icon: const Icon(Icons.menu),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF1B2551),
+          leading: IconButton(
             onPressed: () {},
+            icon: IconButton(
+              icon: const Icon(Icons.menu, color: Colors.white),
+              onPressed: () {},
+            ),
           ),
+
+          title: Image.asset('assets/imagenes/logorojo.png'),
+          centerTitle: true,
+
+          actions: [IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.account_circle, color: Colors.white))],
+
         ),
 
+      body: Column( children:[
 
 
-      ),
+
+      ])
+
+      ,
 
     );
   }
