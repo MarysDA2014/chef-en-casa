e3e2a9c15e1e1af713411811ca9902184e78950c
laura <laura.lara2034@gmail.com>
2023-10-19 20:48:56 -0500

    Cambios Lau

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter layout demo',
-      home: Login (),
+      home: Inicio (),
     );
   }
 }
--- lib/pantallas/inicio.dart
+++ lib/pantallas/inicio.dart
@@ -13,31 +13,58 @@ class _InicioState extends State<Inicio> {
     return Scaffold(
       body: Column(
         children: [
-          Row(children: [
+
+          Container( margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child:
+
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
 
               Container(
-                width: MediaQuery.of(context).size.width * 0.9,
+                width: MediaQuery.of(context).size.width * 0.8,
 
                 child: TextField(
                   decoration: InputDecoration(
                     filled: true,
-                    fillColor: Colors.cyan,
+                    fillColor: Colors.white,
                     prefixIcon: const Icon(Icons.search),
                     focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 1,color: Color(0xffF02E65)),
+                      borderSide: BorderSide(width: 1,color: Color(0xff1B2551)),
                       borderRadius: BorderRadius.circular(30)
                     ),
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 1, color: Color(0xffF02E65)),
+                      borderSide: BorderSide(width: 1, color: Color(0xff1B2551)),
                       borderRadius: BorderRadius.circular(30),
                     )
                   ),
                 ),
-              )
+              ),
 
-            ],)
-        ],
-      )
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      )),
+                  backgroundColor:  MaterialStateProperty.all(Color(0xffEB920D))
+                ),
+                child: Icon( Icons.filter_list,size: 40, color: Colors.white,)
+              ),
+
+            ],),
+          ),
+
+          Container(child:
+
+            Column( children:[
+
+              Material()
+
+            ]),
+          )
+
+        ],)
 
     );
 
